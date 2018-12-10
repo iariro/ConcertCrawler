abf91973418d3eb00839621aa6ea0e538dc9d8ed
kumagai <kumagai@toshiba-tops.co.jp>
2018-12-10 12:52:45 +0900

    ファイル整理

--- concertcrawler_entry_local.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python3
-
-import concertcrawler
-
-master = concertcrawler.loadConcertSchema('testdata/ConcertSchema.xsd')
-urls = concertcrawler.getPastOrchestra()
-concertcrawler.scrapeAllFromFile(master, 'testdata/concertinfo.txt')
