c3325eeceef4378cf101b10b042af511ece3043b
kumagai <iariro2@gol.com>
2019-04-06 04:42:32 +0900

    ファイル分割 続き

--- test/charutiltest.py
+++ test/charutiltest.py
@@ -1,17 +1,17 @@
 import unittest
 import sys
-sys.path.append('..')
-import concertcrawler
+sys.path.append('../lib')
+import concertcrawler_file
 
 from datetime import datetime, timedelta
 
 class MyTest(unittest.TestCase):
 
 	def testZenkakuToHankaku(self):
-		self.assertEqual("123", concertcrawler.zenkakuToHankaku('１２３'))
+		self.assertEqual("123", concertcrawler_file.zenkakuToHankaku('１２３'))
 
 	def testGetKaijouFromKaien(self):
-		info = concertcrawler.ConcertInformation()
+		info = concertcrawler_file.ConcertInformation()
 		info.set('kaien', "14:00")
 		self.assertEqual("13:30", info.getKaijou())
 
