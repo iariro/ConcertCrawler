e079d3802169776d5db2eaee6b3d604f0480ceff
kumagai <iariro@gol.com>
2021-11-15 21:00:00 +0900

    収集結果をLINEに通知 #4

--- concertcrawler.py
+++ concertcrawler.py
@@ -11,8 +11,8 @@ from ftplib import FTP
 import requests
 
 if len(sys.argv) < 3:
-	print('Usage: schemafilepath fileoutdirpath')
-	sys.exit(0)
+    print('Usage: schemafilepath fileoutdirpath')
+    sys.exit(0)
 
 schemafilepath = sys.argv[1]
 fileoutdir = sys.argv[2]
@@ -22,14 +22,14 @@ crawl = True
 upload = True
 
 if crawl:
-	master = concertcrawler_file.loadConcertSchema(schemafilepath)
-	urls = concertcrawler_db.getPastOrchestraFromDB(dbhost)
-	with open(os.path.join(fileoutdir, 'NewConcert.txt'), 'w', encoding='utf-8') as file:
-		root = concertcrawler_net.getTextAllAndOutputFile(master, urls, file)
+    master = concertcrawler_file.loadConcertSchema(schemafilepath)
+    urls = concertcrawler_db.getPastOrchestraFromDB(dbhost)
+    with open(os.path.join(fileoutdir, 'NewConcert.txt'), 'w', encoding='utf-8') as file:
+        root = concertcrawler_net.getTextAllAndOutputFile(master, urls, file)
 
-	with open(os.path.join(fileoutdir, 'NewConcert.xml'), 'w', encoding='utf-8') as xml:
-		reparsed = minidom.parseString(tostring(root, 'utf-8'))
-		xml.write(reparsed.toprettyxml(indent="  "))
+    with open(os.path.join(fileoutdir, 'NewConcert.xml'), 'w', encoding='utf-8') as xml:
+        reparsed = minidom.parseString(tostring(root, 'utf-8'))
+        xml.write(reparsed.toprettyxml(indent="  "))
 
     # notify to LINE
     token = "nPQEoC190nfvydJRbQmY75SY00Ygvt0CxsaXWoLTUUH"
@@ -39,10 +39,10 @@ if crawl:
     requests.post(url, headers=headers, data=payload)
 
 if upload:
-	FTP.encoding = "utf-8"
-	FTP.maxline = 16384
-	ftp = FTP("www2.gol.com", "ip0601170243", passwd="Z#5uqBpt")
-	with open(os.path.join(fileoutdir, 'NewConcert.txt'), "rb") as f:
-		ftp.storlines("STOR /private/data/concert/NewConcert.txt", f)
-	with open(os.path.join(fileoutdir, 'NewConcert.xml'), "rb") as f:
-		ftp.storlines("STOR /private/data/concert/NewConcert.xml", f)
+    FTP.encoding = "utf-8"
+    FTP.maxline = 16384
+    ftp = FTP("www2.gol.com", "ip0601170243", passwd="Z#5uqBpt")
+    with open(os.path.join(fileoutdir, 'NewConcert.txt'), "rb") as f:
+        ftp.storlines("STOR /private/data/concert/NewConcert.txt", f)
+    with open(os.path.join(fileoutdir, 'NewConcert.xml'), "rb") as f:
+        ftp.storlines("STOR /private/data/concert/NewConcert.xml", f)
